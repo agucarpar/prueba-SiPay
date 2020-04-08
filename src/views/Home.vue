dac22379da7651c227faadf5d793dbd370bb5246
root <root@DESKTOP-COVV5F4.localdomain>
2020-04-08 18:13:48 +0200

    add pictures

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,7 +27,7 @@ export default {
     height: 100vh;
     width: 100vw;
     box-sizing: border-box;
-    /* background-image: url('../../public/background.jpg'); */
+    background-image: url('../../public/background.jpg');
     background-size: cover;
   }
   header>h1{
